bb850ded7474b3a81b3ad4221effced689c6bca7
RB-Erik <erik.reindl@redbull.com>
2025-03-14 10:55:59 +0000

    Chellenge 2 completed

--- Payload Specialist Erik/main.tf
+++ Payload Specialist Erik/main.tf
@@ -2,8 +2,24 @@
 resource "azurerm_resource_group" "example" {
   name     = "rg-Erik"
   location = "West Europe"
-  
+
   tags = {
     owner = "erik.reindl@redbull.com"
   }
+}
+
+# Virtual Network
+resource "azurerm_virtual_network" "vnet" {
+  name                = "my-vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+}
+
+# Subnet 1
+resource "azurerm_subnet" "subnet1" {
+  name                 = "subnet-1"
+  resource_group_name  = azurerm_resource_group.example.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.0.0/24"]
 }
