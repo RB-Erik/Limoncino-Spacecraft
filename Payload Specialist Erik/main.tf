d2fef07deca5c1182f3a2d35ae42a099cf408b21
RB-Erik <erik.reindl@redbull.com>
2025-03-14 12:17:41 +0000

    Challenge 3 complted

--- Payload Specialist Erik/main.tf
+++ Payload Specialist Erik/main.tf
@@ -23,3 +23,44 @@ resource "azurerm_subnet" "subnet1" {
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.0.0/24"]
 }
+
+
+# Define the network interface
+
+resource "azurerm_network_interface" "example-nic" {
+  name                = "example-nic"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+
+  ip_configuration {
+    name                          = "example-ipcfg"
+    subnet_id                     = azurerm_subnet.subnet1.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+# Define the virtual machine
+resource "azurerm_linux_virtual_machine" "vm" {
+  name                = "erik-vm"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  network_interface_ids = [
+    azurerm_network_interface.example-nic.id,
+  ]
+  size               = "Standard_DS1_v2"
+  admin_username     = "username"
+  admin_password     = "34FDA$#214f"  # For demonstration purposes only. Use secure methods for production.
+  disable_password_authentication = "false"
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
+  }
+}
