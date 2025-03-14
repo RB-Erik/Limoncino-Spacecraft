32d83f6b0a56eb367aba643a3c0c0cddc1a69b5e
Bapt <achbapt@gmail.com>
2025-03-14 12:31:49 +0000

    VM created successfuly

--- Bapt/main.tf
+++ Bapt/main.tf
@@ -1,7 +1,7 @@
 # Create a resource group
 resource "azurerm_resource_group" "example" {
-  name     = "BaptResourceGroup"
-  location = "West Europe"
+  name     = "BaptResourceGroup2"
+  location = "southeastasia"
 
   tags = {
     owner = "baptiste.achard@redbull.com"
@@ -21,13 +21,42 @@ resource "azurerm_subnet" "example" {
   resource_group_name  = azurerm_resource_group.example.name
   virtual_network_name = azurerm_virtual_network.example.name
   address_prefixes     = ["10.0.1.0/24"]
+}
 
-  delegation {
-    name = "delegation"
+resource "azurerm_network_interface" "example-nic" {
+  name                = "example-nic"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
 
-    service_delegation {
-      name    = "Microsoft.ContainerInstance/containerGroups"
-      actions = ["Microsoft.Network/virtualNetworks/subnets/join/action", "Microsoft.Network/virtualNetworks/subnets/prepareNetworkPolicies/action"]
-    }
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.example.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+# Define the virtual machine
+resource "azurerm_linux_virtual_machine" "vm" {
+  name                = "bapt-vm"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  network_interface_ids = [
+    azurerm_network_interface.example-nic.id,
+  ]
+  size               = "Standard_B1s"
+  admin_username     = "username"
+  admin_password     = "34FDA$#214f"  # For demonstration purposes only. Use secure methods for production.
+  disable_password_authentication = "false"
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
   }
 }
